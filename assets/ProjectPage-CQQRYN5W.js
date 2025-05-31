1c967089f4c691f45ee5069d4a1313bc1356a45a
ars <kigetan747@provko.com>
2025-06-01 01:34:51 +0300

    Updates

--- assets/ProjectPage-CQQRYN5W.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-YVvcq5W7.js";function e(){return r.jsx("h1",{children:"Project"})}export{e as default};
--- assets/ProjectView-CkbV-_n0.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-YVvcq5W7.js";function t(){return e.jsx("h1",{children:"Project View"})}export{t as default};
