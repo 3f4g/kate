0e7ab0c05313e125ef879056f270bae76fd8d7ec
ars <kigetan747@provko.com>
2025-06-01 20:47:51 +0300

    wefiwef

--- src/router.tsx
+++ src/router.tsx
@@ -1,31 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 import Navbar from "./components/Navbar/Navbar";
+import NotFoundRedirect from "./NotFound";
 
 const MainPage = lazy(() => import("./pages/Main/MainPage"));
 const ExibitionsPage = lazy(() => import("./pages/Exibitions/ExibitionsPage"));
 const ProjectPageView = lazy(() => import("./pages/ProjectView/ProjectView"));
-
-// export default function Router() {
-//   return (
-//     <BrowserRouter>
-//       <div className="layout">
-        
-    
-//           <Navbar />
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <Routes>
-//               <Route path="/kate" element={<MainPage />} />
-//               <Route path="kate/projects/:id" element={<ProjectPageView />} />
-//               <Route path="kate/exibitions" element={<ExibitionsPage />} />
-//             </Routes>
-//           </Suspense>
-//         </div>
-    
-//     </BrowserRouter>
-//   );
-// }
-
 
 export default function Router() {
   return (
@@ -38,6 +18,7 @@ export default function Router() {
               <Route path="/kate" element={<MainPage />} />
               <Route path="kate/projects/:id" element={<ProjectPageView />} />
               <Route path="kate/exibitions" element={<ExibitionsPage />} />
+              <Route path="*" element={<NotFoundRedirect />} />
             </Routes>
           </Suspense>
         </div>
